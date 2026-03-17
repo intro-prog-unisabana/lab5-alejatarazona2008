658cb2d44ce6cbce17e60602d53da27b55347e30
Alejandra Tarazona <alejatarazona2008@gmail.com>
2026-03-17 12:34:28 -0500

    utils

---
+++ utils_calc.py
@@ -0,0 +1,43 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+def flip(input_string):
+    return input_string[::-1]
+
+def count_letters(input_string, letter):
+    count = 0
+    for char in input_string:
+        if char == letter:
+            count += 1
+    return count
+# FREEZE CODE END
+
+def add(num1, num2):
+    return num1 + num2
+
+def sub(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 == 0:
+        return "Error: Division by zero is not allowed."
+    return num1 / num2
+
+def exponent(base, exp):
+    return base ** exp
+
+def modulo(num1, num2):
+    if num2 == 0:
+        return "Error: Modulo by zero is not allowed."
+    return num1 % num2
+
+def floor_divide(num1, num2):
+    if num2 == 0:
+        return "Error: Division by zero is not allowed."
+    return num1 // num2
+
+def absolute(num):
+    return abs(num)
