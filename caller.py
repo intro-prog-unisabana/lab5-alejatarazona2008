1d6bee5c8ab2d020747e697c4331821d9014c3d8
Alejandra Tarazona <alejatarazona2008@gmail.com>
2026-03-17 12:31:48 -0500

    caller

--- caller.py
+++ caller.py
@@ -1,8 +1,7 @@
 from mystery_module import transform_data
+
 x = int(input())
 y = float(input())
 
-result = transform_data(x, y, "quiz_test")
-print(result)
-
+print(transform_data(x, y, "quiz_test"))
 
